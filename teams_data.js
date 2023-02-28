08e0be2351b726a8be42acfb958363256b935d1a
aqeeluddin08 <122413610+aqeeluddin08@users.noreply.github.com>
2023-02-28 18:44:24 +0300

    Create teams_data.js

---
+++ teams_data.js
@@ -0,0 +1,38 @@
+db.team.insertMany([
+    {
+       name: "LeBron James",
+       position: "Small Forward",
+       points: 26.7,
+       rebounds: 8.2,
+       assists: 8.3
+    },
+    {
+       name: "Anthony Davis",
+       position: "Power Forward",
+       points: 21.8,
+       rebounds: 7.9,
+       assists: 3.1
+    },
+    {
+       name: "Dennis Schroder",
+       position: "Point Guard",
+       points: 15.4,
+       rebounds: 3.5,
+       assists: 5.8
+    },
+    {
+       name: "Kentavious Caldwell-Pope",
+       position: "Shooting Guard",
+       points: 9.7,
+       rebounds: 2.7,
+       assists: 1.7
+    },
+    {
+       name: "Marc Gasol",
+       position: "Center",
+       points: 5.0,
+       rebounds: 4.1,
+       assists: 2.1
+    }
+ ])
+ 
